61c793eb00e9245bbc5830a5dbac61bea037609e
magic-cucumber <kagg886@qq.com>
2026-03-26 17:57:17 +0800

    page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
+  base: process.env.BASE_PATH ?? '/',
   plugins: [vue()],
-})
+}))
